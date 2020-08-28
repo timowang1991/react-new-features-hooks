785349603d6b19943c5913177ba77ccbf6134606
Timo <timowang1991@gmail.com>
2020-08-28 21:39:57 +0800

    add setText useState

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import * as serviceWorker from './serviceWorker';
 
 const App = (props) => {
     const [count, setCount] = useState(props.count)
+    const [text, setText] = useState('')
 
     return (
         <div>
-            <p>The current count is {count}</p>
+            <p>The current {text || 'count'} is {count}</p>
             <button onClick={() => setCount(count + 1)}>+1</button>
             <button onClick={() => setCount(count - 1)}>-1</button>
             <button onClick={() => setCount(0)}>reset</button>
+            <input value={text} onChange={(e) => setText(e.target.value)} />
         </div>
     )
 }
