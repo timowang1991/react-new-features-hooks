4b6a3e733f8ecb8d971ce3bfca0686679ab0ac34
Timo <timowang1991@gmail.com>
2020-08-28 21:30:15 +0800

    useState

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const App = (props) => {
+    const [count, setCount] = useState(props.count)
+
+    return (
+        <div>
+            <p>The current count is {count}</p>
+            <button onClick={() => setCount(count + 1)}>+1</button>
+            <button onClick={() => setCount(count - 1)}>-1</button>
+            <button onClick={() => setCount(0)}>reset</button>
+        </div>
+    )
+}
+
+App.defaultProps = {
+    count: 0
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App count={2}></App>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
