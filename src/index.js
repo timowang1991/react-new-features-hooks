8edb618ed525ab240c9a5b4751db1c9d542d0891
Timo <timowang1991@gmail.com>
2020-08-29 15:27:00 +0800

    useEffect AppNote

--- src/index.js
+++ src/index.js
@@ -31,53 +31,60 @@ App.defaultProps = {
     count: 0
 }
 
-// const NoteApp = () => {
-//     const notesData = JSON.parse(localStorage.getItem('notes') || '[]')
-//     const [notes, setNotes] = useState(notesData || [])
-//     const [title, setTitle] = useState('')
-//     const [body, setBody] = useState('')
+const NoteApp = () => {
+    const [notes, setNotes] = useState([])
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
 
-//     useEffect(() => {
-//         localStorage.setItem('notes', JSON.stringify(notes));
-//     })
+    useEffect(() => {
+        const notesData = JSON.parse(localStorage.getItem('notes') || '[]')
+        if (notesData) {
+            setNotes(notesData)
+        }
+    }, [])
 
-//     const addNote = (e) => {
-//         e.preventDefault()
-//         setNotes([
-//             ...notes,
-//             { title, body }
-//         ])
-//         setTitle('')
-//         setBody('')
-//     }
+    useEffect(() => {
+        localStorage.setItem('notes', JSON.stringify(notes));
+    }, [notes])
 
-//     const removeNote = (title) => {
-//         setNotes(notes.filter((note) => note.title !== title))
-//     }
+    const addNote = (e) => {
+        e.preventDefault()
+        setNotes([
+            ...notes,
+            { title, body }
+        ])
+        setTitle('')
+        setBody('')
+    }
 
-//     return (
-//         <div>
-//             <h1>Notes</h1>
-//             {notes.map((note) => (
-//                 <div key={note.title}>
-//                     <h3>{note.title}</h3>
-//                     <p>{note.body}</p>
-//                     <button onClick={() => removeNote(note.title)}>x</button>
-//                 </div>
-//             ))}
-//             <p>Add Note</p>
-//             <form onSubmit={addNote}>
-//                 <input value={title} onChange={(e) => setTitle(e.target.value)} />
-//                 <textarea value={body} onChange={(e) => setBody(e.target.value)} />
-//                 <button>add note</button>
-//             </form>
-//         </div>
-//     )
-// }
+    const removeNote = (title) => {
+        setNotes(notes.filter((note) => note.title !== title))
+    }
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            {notes.map((note) => (
+                <div key={note.title}>
+                    <h3>{note.title}</h3>
+                    <p>{note.body}</p>
+                    <button onClick={() => removeNote(note.title)}>x</button>
+                </div>
+            ))}
+            <p>Add Note</p>
+            <form onSubmit={addNote}>
+                <input value={title} onChange={(e) => setTitle(e.target.value)} />
+                <textarea value={body} onChange={(e) => setBody(e.target.value)} />
+                <button>add note</button>
+            </form>
+        </div>
+    )
+}
 
 ReactDOM.render(
     <React.StrictMode>
         <App></App>
+        <NoteApp></NoteApp>
     </React.StrictMode>,
     document.getElementById('root')
 );
