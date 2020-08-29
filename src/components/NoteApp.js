43d6c9771b3917119a73055b28634037d5d65845
Timo <timowang1991@gmail.com>
2020-08-29 18:51:36 +0800

    use context

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -3,6 +3,8 @@ import notesReducer from '../reducers/notes'
 import NoteList from './NoteList'
 import AddNoteForm from './AddNoteForm'
 
+import NotesContext from '../context/notes-context'
+
 const NoteApp = () => {
     const [notes, dispatch] = useReducer(notesReducer, [])
     
@@ -17,16 +19,12 @@ const NoteApp = () => {
         localStorage.setItem('notes', JSON.stringify(notes));
     }, [notes])
 
-    const removeNote = (title) => {
-        dispatch({ type: 'REMOVE_NOTE', title })
-    }
-
     return (
-        <div>
+        <NotesContext.Provider value={{ notes, dispatch }}>
             <h1>Notes</h1>
-            <NoteList notes={notes} removeNote={removeNote} />
-            <AddNoteForm dispatch={dispatch} />
-        </div>
+            <NoteList />
+            <AddNoteForm />
+        </NotesContext.Provider>
     )
 }
 
