18286f000534ce80ab7ec9d6f82638509eafe597
Timo <timowang1991@gmail.com>
2020-08-29 15:14:13 +0800

    useEffect NotesApp

--- src/index.js
+++ src/index.js
@@ -2,72 +2,77 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const App = (props) => {
-    const [count, setCount] = useState(props.count)
-    const [text, setText] = useState('')
+// const App = (props) => {
+//     const [count, setCount] = useState(props.count)
+//     const [text, setText] = useState('')
 
-    useEffect(() => {
-        console.log('useEffect ran')
-        document.title = count
-    })
-
-    return (
-        <div>
-            <p>The current {text || 'count'} is {count}</p>
-            <button onClick={() => setCount(count + 1)}>+1</button>
-            <button onClick={() => setCount(count - 1)}>-1</button>
-            <button onClick={() => setCount(0)}>reset</button>
-            <input value={text} onChange={(e) => setText(e.target.value)} />
-        </div>
-    )
-}
-
-App.defaultProps = {
-    count: 0
-}
-
-// const NoteApp = () => {
-//     const [notes, setNotes] = useState([])
-//     const [title, setTitle] = useState('')
-//     const [body, setBody] = useState('')
-
-//     const addNote = (e) => {
-//         e.preventDefault()
-//         setNotes([
-//             ...notes,
-//             { title, body }
-//         ])
-//         setTitle('')
-//         setBody('')
-//     }
-
-//     const removeNote = (title) => {
-//         setNotes(notes.filter((note) => note.title !== title))
-//     }
+//     useEffect(() => {
+//         console.log('useEffect ran')
+//         document.title = count
+//     })
 
 //     return (
 //         <div>
-//             <h1>Notes</h1>
-//             {notes.map((note) => (
-//                 <div key={note.title}>
-//                     <h3>{note.title}</h3>
-//                     <p>{note.body}</p>
-//                     <button onClick={() => removeNote(note.title)}>x</button>
-//                 </div>
-//             ))}
-//             <p>Add Note</p>
-//             <form onSubmit={addNote}>
-//                 <input value={title} onChange={(e) => setTitle(e.target.value)} />
-//                 <textarea value={body} onChange={(e) => setBody(e.target.value)} />
-//                 <button>add note</button>
-//             </form>
+//             <p>The current {text || 'count'} is {count}</p>
+//             <button onClick={() => setCount(count + 1)}>+1</button>
+//             <button onClick={() => setCount(count - 1)}>-1</button>
+//             <button onClick={() => setCount(0)}>reset</button>
+//             <input value={text} onChange={(e) => setText(e.target.value)} />
 //         </div>
 //     )
 // }
 
+// App.defaultProps = {
+//     count: 0
+// }
+
+const NoteApp = () => {
+    const notesData = JSON.parse(localStorage.getItem('notes') || '[]')
+    const [notes, setNotes] = useState(notesData || [])
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+
+    useEffect(() => {
+        localStorage.setItem('notes', JSON.stringify(notes));
+    })
+
+    const addNote = (e) => {
+        e.preventDefault()
+        setNotes([
+            ...notes,
+            { title, body }
+        ])
+        setTitle('')
+        setBody('')
+    }
+
+    const removeNote = (title) => {
+        setNotes(notes.filter((note) => note.title !== title))
+    }
+
+    return (
+        <div>
+            <h1>Notes</h1>
+            {notes.map((note) => (
+                <div key={note.title}>
+                    <h3>{note.title}</h3>
+                    <p>{note.body}</p>
+                    <button onClick={() => removeNote(note.title)}>x</button>
+                </div>
+            ))}
+            <p>Add Note</p>
+            <form onSubmit={addNote}>
+                <input value={title} onChange={(e) => setTitle(e.target.value)} />
+                <textarea value={body} onChange={(e) => setBody(e.target.value)} />
+                <button>add note</button>
+            </form>
+        </div>
+    )
+}
+
 ReactDOM.render(
     <React.StrictMode>
-        <App></App>
+        <NoteApp></NoteApp>
     </React.StrictMode>,
     document.getElementById('root')
 );
